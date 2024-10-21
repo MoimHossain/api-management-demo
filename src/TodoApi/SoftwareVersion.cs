c1646ae51106cf537070fdc6b9267e5b8270c0ef
Moim HA <moimhossain@gmail.com>
2024-10-21 09:55:10 +0200

    Update software version response to reflect new version number

--- src/TodoApi/SoftwareVersion.cs
+++ src/TodoApi/SoftwareVersion.cs
@@ -14,7 +14,7 @@ namespace TodoApi
                     typeof(Program).Assembly.GetName().Version?.ToString(),
                     FileVersionInfo.GetVersionInfo(typeof(Program).Assembly.Location).FileVersion,
                     FileVersionInfo.GetVersionInfo(typeof(Program).Assembly.Location).ProductVersion,
-                    8);
+                    10);
 
                 todoApiGroup.MapGet("/", () => softwareVersionResponse).WithName("SoftwareVersion").WithOpenApi();                
             }
