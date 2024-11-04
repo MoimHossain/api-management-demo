fad33012d71d8c292b9944b3e86726a47ca582f6
Moim HA <moimhossain@gmail.com>
2024-11-04 09:42:18 +0100

    Update software version to 12 in SoftwareVersion.cs

--- src/TodoApi/SoftwareVersion.cs
+++ src/TodoApi/SoftwareVersion.cs
@@ -14,7 +14,7 @@ namespace TodoApi
                     typeof(Program).Assembly.GetName().Version?.ToString(),
                     FileVersionInfo.GetVersionInfo(typeof(Program).Assembly.Location).FileVersion,
                     FileVersionInfo.GetVersionInfo(typeof(Program).Assembly.Location).ProductVersion,
-                    11);
+                    12);
 
                 todoApiGroup.MapGet("/", () => softwareVersionResponse).WithName("SoftwareVersion").WithOpenApi();                
             }
